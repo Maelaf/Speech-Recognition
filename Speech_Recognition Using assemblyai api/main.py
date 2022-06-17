2e02e2951a1c32da90b8c78264adecf96843c37a
Maelaf <maelafes@gmail.com>
2022-06-17 22:21:24 +0300

    speech to text using assembly ai

---
+++ Speech_Recognition Using assemblyai api/main.py
@@ -0,0 +1,9 @@
+import requests
+
+#Upload the local file to assebly ai
+
+# Start the transcription process
+
+# keep pulling the api till the transcription is done
+
+#save the transcription to a file
