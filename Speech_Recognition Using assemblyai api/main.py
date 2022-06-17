8256f1b8b0ac2d6d725eae9388f567d5753e594a
Maelaf <maelafes@gmail.com>
2022-06-17 22:31:35 +0300

    audio file added to the directory

--- Speech_Recognition Using assemblyai api/main.py
+++ Speech_Recognition Using assemblyai api/main.py
@@ -1,6 +1,27 @@
 import requests
+from api_secrets import API_KEY_ASSEMBLYAI
+import sys
 
 #Upload the local file to assebly ai
+
+upload_endpoint = "https://api.assemblyai.com/v2/upload"
+filename =sys.argv[1]
+
+def read_file(filename, chunk_size=5242880):
+    with open(filename, 'rb') as _file:
+        while True:
+            data = _file.read(chunk_size)
+            if not data:
+                break
+            yield data
+
+headers = {'authorization': "API_KEY_ASSE<BLYAI"}
+response = requests.post(upload_endpoint,
+                        headers=headers,
+                        data=read_file(filename))
+
+print(response.json())
+
 
 # Start the transcription process
 
