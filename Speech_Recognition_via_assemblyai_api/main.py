9332eff7f1d9491057e9dfa59605627528da504e
Maelaf <maelafes@gmail.com>
2022-06-17 22:59:47 +0300

    modularize main.py

--- Speech_Recognition_via_assemblyai_api/main.py
+++ Speech_Recognition_via_assemblyai_api/main.py
@@ -3,36 +3,41 @@ from api_secrets import API_KEY_ASSEMBLYAI
 import sys
 
 #Upload the local file to assebly ai
-
+headers = {'authorization': API_KEY_ASSEMBLYAI}
 upload_endpoint = "https://api.assemblyai.com/v2/upload"
 transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
 filename =sys.argv[1]
+def upload():
+    def read_file(filename, chunk_size=5242880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
 
-def read_file(filename, chunk_size=5242880):
-    with open(filename, 'rb') as _file:
-        while True:
-            data = _file.read(chunk_size)
-            if not data:
-                break
-            yield data
 
-headers = {'authorization': API_KEY_ASSEMBLYAI}
-response = requests.post(upload_endpoint,
-                        headers=headers,
-                        data=read_file(filename))
+    upload_response = requests.post(upload_endpoint,
+                            headers=headers,
+                            data=read_file(filename))
+    audio_url = upload_response.json()['upload_url']
+    
+    return audio_url
 
-print(response.json())
+
 
 
 # Start the transcription process
 
-audio_url = response.json()['upload_url']
 
-json = { "audio_url": audio_url }
+def transcribe():
+    trancript_request = { "audio_url": audio_url }
 
-response = requests.post(transcript_endpoint, json=json, headers=headers)
-print(response.json()
+    transcript_response = requests.post(transcript_endpoint, json=trancript_request, headers=headers)
+    
+    job_id = trancript_request.json(['id'])
+    return job_id
 
-# keep pulling the api till the transcription is done
+# keep polling the api till the transcription is done
 
 #save the transcription to a file
