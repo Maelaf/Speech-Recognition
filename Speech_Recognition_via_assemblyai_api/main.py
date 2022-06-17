201eafc6aaced2fa680e7c304f5f1a493b3fe455
Maelaf <maelafes@gmail.com>
2022-06-17 22:49:49 +0300

    starting the transcription process

--- Speech_Recognition_via_assemblyai_api/main.py
+++ Speech_Recognition_via_assemblyai_api/main.py
@@ -5,6 +5,7 @@ import sys
 #Upload the local file to assebly ai
 
 upload_endpoint = "https://api.assemblyai.com/v2/upload"
+transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
 filename =sys.argv[1]
 
 def read_file(filename, chunk_size=5242880):
@@ -15,7 +16,7 @@ def read_file(filename, chunk_size=5242880):
                 break
             yield data
 
-headers = {'authorization': 'API_KEY_ASSEBLYAI'}
+headers = {'authorization': API_KEY_ASSEMBLYAI}
 response = requests.post(upload_endpoint,
                         headers=headers,
                         data=read_file(filename))
@@ -24,6 +25,13 @@ print(response.json())
 
 
 # Start the transcription process
+
+audio_url = response.json()['upload_url']
+
+json = { "audio_url": audio_url }
+
+response = requests.post(transcript_endpoint, json=json, headers=headers)
+print(response.json()
 
 # keep pulling the api till the transcription is done
 
